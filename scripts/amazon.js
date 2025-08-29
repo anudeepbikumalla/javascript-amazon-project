e981e48322c130dfe574183b98c63b6f38386f8a
deep09 <bikumallaanudeep@gmail.com>
2025-08-29 08:05:55 -0400

    make the cart quantity interactve

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,12 @@ productsHTML +=  `<div class="product-container">
         quantity:1
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+   
      });
       });
